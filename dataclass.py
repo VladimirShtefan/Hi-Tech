18651aa9aeee4e4a0ab8413e5e2e69908df7f9ef
VladmimirShtefan <shtefanvladimir14@gmail.com>
2023-02-04 21:47:19 +0700

    рефактор

--- dataclass.py
+++ dataclass.py
@@ -1,5 +1,6 @@
-from pydantic.main import BaseModel
 from pydantic.dataclasses import dataclass as pyd_dataclass
+
+from config import MAX_SUM
 
 
 @pyd_dataclass(eq=True, frozen=True)
@@ -11,8 +12,31 @@ class Coordinate:
 @pyd_dataclass(eq=True, frozen=True)
 class Node:
     coordinate: Coordinate
-    weight: int
 
+    @property
+    def neighbours(self):
+        neighbours = []
+        coordinate = Coordinate(x=self.coordinate.x - 1, y=self.coordinate.y)
+        if self.coordinate.x != 0 and self.point_is_reachable(coordinate):
+            neighbours.append(coordinate)
 
-class NeighboursNodes(BaseModel):
-    neighbours: list[Node, ...] | None
+        coordinate = Coordinate(x=self.coordinate.x + 1, y=self.coordinate.y)
+        if self.point_is_reachable(coordinate):
+            neighbours.append(coordinate)
+
+        coordinate = Coordinate(x=self.coordinate.x, y=self.coordinate.y - 1)
+        if self.coordinate.y != 0 and self.point_is_reachable(coordinate):
+            neighbours.append(coordinate)
+
+        coordinate = Coordinate(x=self.coordinate.x, y=self.coordinate.y + 1)
+        if self.point_is_reachable(coordinate):
+            neighbours.append(coordinate)
+
+        return tuple(neighbours)
+
+    @staticmethod
+    def get_weight(coordinate: Coordinate):
+        return sum([int(num) for num in ''.join(map(str, (coordinate.x, coordinate.y)))])
+
+    def point_is_reachable(self, coordinate):
+        return self.get_weight(coordinate) <= MAX_SUM
--- grid_on_dataclasses.py
+++ grid_on_dataclasses.py
@@ -1,6 +1,7 @@
 from networkx import Graph
 
-from dataclass import Node, Coordinate, NeighboursNodes
+from config import START_POINT
+from dataclass import Node, Coordinate
 
 
 class Field:
@@ -13,54 +14,7 @@ class Field:
         """
         Для установки начальной вершины (узла)
         """
-        target: Node = Node(coordinate=self.start_point, weight=self.sum_digits_in_coordinates(self.start_point))
-        self.g.add_node(target)
-
-    def get_neighbours_node(self, node: Node) -> NeighboursNodes:
-        """
-        Для формирования соседних доступных узлов
-        :param node: текущий узел
-        :return: соседние узлы
-        """
-        neighbours = NeighboursNodes(neighbours=[])
-
-        up_coordinates = Coordinate(x=node.coordinate.x - 1, y=node.coordinate.y)
-        if node.coordinate.x != 0 and self.point_is_reachable(up_coordinates):
-            neighbours.neighbours.append(Node(coordinate=up_coordinates, weight=node.weight - 1))
-
-        down_coordinates = Coordinate(x=node.coordinate.x + 1, y=node.coordinate.y)
-        if self.point_is_reachable(down_coordinates):
-            neighbours.neighbours.append(Node(coordinate=down_coordinates, weight=node.weight + 1))
-
-        left_coordinates = Coordinate(x=node.coordinate.x, y=node.coordinate.y - 1)
-        if node.coordinate.y != 0 and self.point_is_reachable(left_coordinates):
-            neighbours.neighbours.append(Node(coordinate=left_coordinates, weight=node.weight - 1))
-
-        right_coordinates = Coordinate(x=node.coordinate.x, y=node.coordinate.y + 1)
-        if self.point_is_reachable(right_coordinates):
-            neighbours.neighbours.append(Node(coordinate=right_coordinates, weight=node.weight + 1))
-
-        return NeighboursNodes(neighbours=neighbours.neighbours)
-
-    def sum_digits_in_coordinates(self, coordinates: Coordinate) -> int:
-        """
-        Для подсчета суммы цифр в координатах
-        :param coordinates: координаты
-        :return: сумма цифр в координатах
-        """
-        return self.sum_digits(''.join(map(str, (coordinates.x, coordinates.y))))
-
-    @staticmethod
-    def sum_digits(number: str) -> int:
-        return sum([int(num) for num in number])
-
-    def point_is_reachable(self, coordinates: Coordinate) -> bool:
-        """
-        Проверка на доступность координаты в соответствии с max_sum
-        :param coordinates: координаты
-        :return: координаты или при недоступности None
-        """
-        return self.sum_digits_in_coordinates(coordinates) <= self.max_sum
+        self.g.add_node(Node(coordinate=self.start_point))
 
     def search_nodes(self):
         """
@@ -75,14 +29,15 @@ class Field:
             if nodes_count == offset:
                 break
             for node in actual_nodes[offset:]:
-                for cell in self.get_neighbours_node(node).neighbours:
-                    if cell:
-                        self.g.add_node(cell)
-                        self.g.add_edge(cell, node)
+                neighbours = Node(coordinate=node.coordinate).neighbours
+                for neighbour in neighbours:
+                    new_node = Node(coordinate=neighbour)
+                    self.g.add_node(new_node)
+                    self.g.add_edge(new_node, node)
             offset = nodes_count
         return len(self.g.nodes)
 
 
 if __name__ == '__main__':
-    field = Field(1000, 1000)
+    field = Field(*START_POINT)
     print(field.search_nodes())
